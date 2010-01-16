22022611d066508a3fd54b03a979567717ab3e20
Austin Bales <arbales@gmail.com>
2010-01-16 00:55:39 -0500

    Manual class method inheritance is the only way it works.

--- cafemapper.js
+++ cafemapper.js
@@ -4,38 +4,84 @@ CafeMapper.activate = function(){
 		var e = new Element("div", {id: "__barista__"})
 		e.style.display = "none";
 		document.body.appendChild(e);
+		CafeMapper.element = e;
 	}
 }
 CafeMapper.Model = {}
 
 CafeMapper.DataObject = Class.create({
+	
+	/**
+	   *  new CafeMapper.DataObject(data)
+	   *  - data (Object): An object/ Hash of information
+	   *  to populate the object with.
+	   *
+	   *  Creates a new `CafeMapper.DataObject`.
+	  **/
 	initialize: function(data){
 		Object.extend(this, data);
+
+		// Set/Reset the expiration date as a string an as an Date object.
 		if (this.expires_at == undefined){
 			this._expires_at = (1).minutes().fromNow().toString();
 			this.expires_at = Date.parse(this._expires_at);
   		}
-		if (this._refresh == true){
-			this.store();
-			Reservation.refresh(this);
-		} else {
-			if (this.expires_at < new Date()){
-			    CafeMapper.DataObject.refresh(this);
-			} else {
-			}
+		if (!this.id){this.model.uuid = uuid();}
+								
+		// If the the object has expired, refresh it.
+		if (this.expires_at < new Date() && (typeof this.refresh == 'function')){
+		    this.refresh(this);
 		}
-	}
-}); 
+	},
+	store: function(){
+		if (this.id != undefined){
+			return localStorage.setItem(this.model.klass+'.'+this.id, JSON.stringify(this));
+		}else{
+			return localStorage.setItem(this.model.klass+'.'+this.model.uuid, JSON.stringify(this));
+		}
+	},
 
-CafeMapper.Model.initAs = function(name, options){     
+});
+
+CafeMapper.ServerCommunication = {
+	refresh: function(){                                                      
+		CafeMapper.DataObject.refresh(this);
+		CafeMapper.element.fire("refreshing:"+this.model.klass.toLowerCase()+"."+this.id); 
+	}
+}
+
+CafeMapper.ElementMethods = {
+	wakeDataObject: function(element){   
+		element = $(element);
+		return Reservation.get(element.retrieve('reservation_id'));
+	},
+	linkBooking: function(element, reservation_id){  
+		element = $(element);
+		element.store('reservation_id', reservation_id);
+		element.addClassName('_reservation');
+		id = reservation_id;       
+		elementid = element.identify();
+		$('app_element').observe(("updated:reservation."+id), function(){
+			Reservation.update_element(id, elementid);
+		});
+	}	
+};
+
+CafeMapper.Model.initAs = function(klass, options){     
 	_options = {
-		_model: {
-			name: name,
+		model: {
+			klass: klass,
+			serverPrefix: "",
 		},
 	}
 	Object.extend(_options, options);
+	_options.model.klass = klass;
 	var c = Class.create(CafeMapper.DataObject, _options);
-//	Object.extend(Reservation, CafeMapper.DataObject); 
+	c.klassName = klass;
+	c.get = CafeMapper.DataObject.get
+	c.createStub = CafeMapper.DataObject.createStub
+	c.initObjectWithData = CafeMapper.DataObject.initObjectWithData
+	c.include = c.addMethods
 	return c
 }
 
@@ -43,11 +89,34 @@ CafeMapper.Model.initAs = function(name, options){
 $MODEL = CafeMapper.Model.initAs 
 
 CafeMapper.DataObject.refresh = function(dobject){
-	new Ajax.Request('/booking/'+dobject.id+".json", {
+	new Ajax.Request(dobject.model.serverPrefix+'/'+dobject.model.klass.toLowerCase()+"/"+dobject.id+".json", {
 		method: "get",
 		onSuccess: function(transport){
 			var data = transport.responseText.evalJSON();
 			booking.receiveUpdate(data);
+		},
+		onFailure: function(){
+			console.warn("Unable to refresh object.");
 		}
 	});
-}                     	
+}
+
+CafeMapper.DataObject.initObjectWithData = function(data){
+	var o = new this(data);
+	o.store();
+	return o;
+}
+
+CafeMapper.DataObject.createStub = function(id){ 
+	return this.initObjectWithData(this.stub);
+}
+
+CafeMapper.DataObject.get = function(id){ 
+   var id = id;                               
+   var data = JSON.parse(localStorage.getItem(this.klassName+'.'+id));
+	if (data == null){
+		return this.stub(id);
+	} else {
+		return this.initObjectWithData(data);
+	}
+}
