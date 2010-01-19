4c39848c6d8575a2d8cd3f37e258286276793279
Austin Bales <arbales@gmail.com>
2010-01-19 00:41:01 -0500

    Added documentation.

---
+++ docs/javascripts/item_index.js
@@ -0,0 +1,24 @@
+if (!window.PDoc) window.PDoc = {};
+PDoc.elements = {
+  'Library section': { 'name': 'Library section', 'type': 'section', 'path': 'library/' },
+'CafeMapper': { 'name': 'CafeMapper', 'type': 'class', 'path': 'library/cafemapper/' },
+'CafeMapper.activate': { 'name': 'CafeMapper.activate', 'type': 'class method', 'path': 'library/cafemapper/activate/' },
+'CafeMapper.generate_uuid': { 'name': 'CafeMapper.generate_uuid', 'type': 'class method', 'path': 'library/cafemapper/generate_uuid/' },
+'CafeMapper.DataObject': { 'name': 'CafeMapper.DataObject', 'type': 'class', 'path': 'library/cafemapper/dataobject/' },
+'CafeMapper.DataObject#identify': { 'name': 'CafeMapper.DataObject#identify', 'type': 'instance property', 'path': 'library/cafemapper/dataobject/prototype/identify/' },
+'CafeMapper.DataObject#store': { 'name': 'CafeMapper.DataObject#store', 'type': 'instance method', 'path': 'library/cafemapper/dataobject/prototype/store/' },
+'CafeMapper.DataObject#STORE': { 'name': 'CafeMapper.DataObject#STORE', 'type': 'instance method', 'path': 'library/cafemapper/dataobject/prototype/store/' },
+'CafeMapper.DataObject.initAs': { 'name': 'CafeMapper.DataObject.initAs', 'type': 'class method', 'path': 'library/cafemapper/dataobject/initas/' },
+'$MODEL': { 'name': '$MODEL', 'type': 'utility', 'path': 'library/dollarmodel/' },
+'CafeMapper.DataObject.initWithData': { 'name': 'CafeMapper.DataObject.initWithData', 'type': 'class method', 'path': 'library/cafemapper/dataobject/initwithdata/' },
+'CafeMapper.DataObject.createStub': { 'name': 'CafeMapper.DataObject.createStub', 'type': 'class method', 'path': 'library/cafemapper/dataobject/createstub/' },
+'CafeMapper.DataObject.include': { 'name': 'CafeMapper.DataObject.include', 'type': 'class method', 'path': 'library/cafemapper/dataobject/include/' },
+'CafeMapper.DataObject.get': { 'name': 'CafeMapper.DataObject.get', 'type': 'class method', 'path': 'library/cafemapper/dataobject/get/' },
+'CafeMapper.DataObject.ServerCommunication': { 'name': 'CafeMapper.DataObject.ServerCommunication', 'type': 'mixin', 'path': 'library/cafemapper/dataobject/servercommunication/' },
+'CafeMapper.DataObject.ServerCommunication#receiveUpdate': { 'name': 'CafeMapper.DataObject.ServerCommunication#receiveUpdate', 'type': 'instance method', 'path': 'library/cafemapper/dataobject/servercommunication/prototype/receiveupdate/' },
+'CafeMapper.Modules': { 'name': 'CafeMapper.Modules', 'type': 'namespace', 'path': 'library/cafemapper/modules/' },
+'CafeMapper.Modules.ElementMethods': { 'name': 'CafeMapper.Modules.ElementMethods', 'type': 'mixin', 'path': 'library/cafemapper/modules/elementmethods/' },
+'CafeMapper.DataObject.ElementMethods': { 'name': 'CafeMapper.DataObject.ElementMethods', 'type': 'mixin', 'path': 'library/cafemapper/dataobject/elementmethods/' },
+'CafeMapper.DataObject.ElementMethods.update_element': { 'name': 'CafeMapper.DataObject.ElementMethods.update_element', 'type': 'class method', 'path': 'library/cafemapper/dataobject/elementmethods/update_element/' },
+'CafeMapper.DataObject.refresh': { 'name': 'CafeMapper.DataObject.refresh', 'type': 'class method', 'path': 'library/cafemapper/dataobject/refresh/' }
+};
